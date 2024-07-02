fd2d6495fde2c20d51b9456bf0d5361c7f277a5c
Akinola idris <kakamuftau@gmail.com>
2024-07-02 11:14:55 +0100

    updated

--- firstapi/urls.py
+++ firstapi/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from . import views
+from firstapi import views
 
 urlpatterns = [
-    path('api/hello/', views.hello_view, name='hello_view'),
-    path('api/hello1/', views.intro_view, name='intro_view'),
     path('api/hello/', views.hello, name='hello'),
 ]
 
