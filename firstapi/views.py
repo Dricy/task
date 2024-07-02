04dec4de67f50d11d1375197105801eee92d11c5
Akinola idris <kakamuftau@gmail.com>
2024-07-02 13:58:03 +0100

    updated

--- firstapi/views.py
+++ firstapi/views.py
@@ -2,9 +2,17 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_GET
 import requests
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
 def visit(request):
     visitor_name = request.GET.get('visitor_name', '')
-    client_ip = request.META.get('REMOTE_ADDR', '')
+    client_ip = get_client_ip(request)
     
     # Fetch location and weather using a free API (like OpenWeatherMap)
     weather_api_key = 'b8f58ffa031a3ba694ea099e9ea25576'
