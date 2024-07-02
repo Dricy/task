2b65e34f0b31edad044ebfe951e1118a5add92d5
Akinola idris <kakamuftau@gmail.com>
2024-07-02 09:40:58 +0100

    committed

--- firstapi/urls.py
+++ firstapi/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('api/hello/', views.hello_view, name='hello_view'),
     path('api/hello1/', views.intro_view, name='intro_view'),
+    path('api/hello/', views.hello, name='hello'),
 ]
 
