8e59d5d603ef8d29b5b559564fac772b11f54786
Akinola idris <kakamuftau@gmail.com>
2024-07-02 08:58:34 +0100

    updated

--- task1/wsgi.py
+++ task1/wsgi.py
@@ -7,10 +7,27 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+#import os
+
+#from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task1.settings')
+
+#application = get_wsgi_application()
+
 import os
+import sys
 
+# Path to your Django project directory
+path = 'C:\Users\eedri\tasks\task1\task1'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Set Django settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
+
+# Activate virtual environment
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task1.settings')
-
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
